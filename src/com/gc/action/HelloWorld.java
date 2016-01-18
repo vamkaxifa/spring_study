a3287efc56e822d237db56d0e5fd352f425c0116
vamlichunyan <vamlichunyan@foxmail.com>
2016-01-18 17:22:09 +0800

    集合的依赖注入

--- src/com/gc/action/HelloWorld.java
+++ src/com/gc/action/HelloWorld.java
@@ -1,11 +1,41 @@
 package com.gc.action;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class HelloWorld {
 
 	private String msg = null;
 	private Date date = null;
+	private List alist = null;
+	private Set aset = null;
+	private Map amap = null;
+
+	public List getAlist() {
+		return alist;
+	}
+
+	public void setAlist(List alist) {
+		this.alist = alist;
+	}
+
+	public Set getAset() {
+		return aset;
+	}
+
+	public void setAset(Set aset) {
+		this.aset = aset;
+	}
+
+	public Map getAmap() {
+		return amap;
+	}
+
+	public void setAmap(Map amap) {
+		this.amap = amap;
+	}
 
 	public Date getDate() {
 		return date;
